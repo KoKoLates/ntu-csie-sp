be2fc52e4f08dc4d80bd10d30bbb4f059b6257ee
KoKoLates <the21515@gmail.com>
2025-01-06 19:23:23 +0800

    feat(hw1): update `sample_judger` and assignment description

---
+++ Hw1/README.md
@@ -0,0 +1,5 @@
+# HW1: Booking
+The system is comprised of read servers and write servers. Both can access a file `bookingRecord` that records information about users’ bookings. When a server gets a request from a client, it will respond according to the file’s content. A read server tells the client how many items it has booked. A write server can modify the file to book more or less items.
+
+## Reference
+* [Assignment](https://hackmd.io/@GTooth/By12six1j)
---
+++ Hw1/server.c
@@ -0,0 +1,61 @@
+
+
+int main(int argc, char **argv) {
+
+    return 0;
+}
+
+
+// ======================================================================================================
+// You don't need to know how the following codes are working
+
+static void init_request(request* reqP) {
+    reqP->conn_fd = -1;
+    reqP->buf_len = 0;
+    reqP->id = 0;
+    reqP->wait_for_write = 0;
+}
+
+static void free_request(request* reqP) {
+    init_request(reqP);
+}
+
+static void init_server(unsigned short port) {
+    struct sockaddr_in servaddr;
+    int tmp;
+
+    gethostname(svr.hostname, sizeof(svr.hostname));
+    svr.port = port;
+
+    svr.listen_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (svr.listen_fd < 0) ERR_EXIT("socket");
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+    tmp = 1;
+    if (setsockopt(svr.listen_fd, SOL_SOCKET, SO_REUSEADDR, (void*)&tmp, sizeof(tmp)) < 0) {
+        ERR_EXIT("setsockopt");
+    }
+    if (bind(svr.listen_fd, (struct sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
+        ERR_EXIT("bind");
+    }
+    if (listen(svr.listen_fd, 1024) < 0) {
+        ERR_EXIT("listen");
+    }
+
+    // Get file descripter table size and initialize request table
+    maxfd = getdtablesize();
+    requestP = (request*) malloc(sizeof(request) * maxfd);
+    if (requestP == NULL) {
+        ERR_EXIT("out of memory allocating all requests");
+    }
+    for (int i = 0; i < maxfd; i++) {
+        init_request(&requestP[i]);
+    }
+    requestP[svr.listen_fd].conn_fd = svr.listen_fd;
+    strcpy(requestP[svr.listen_fd].host, svr.hostname);
+
+    return;
+}
