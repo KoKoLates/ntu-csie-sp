011a1ea9b221a057fd96f27b756377626b5f71ab
KoKoLates <the21515@gmail.com>
2025-03-09 22:34:22 +0800

    refactor(hw4): macro and definition of server

--- Hw4/server.c
+++ Hw4/server.c
@@ -1,92 +1,242 @@
 #include "header.h"
-#include "mylib.h"
 
-const unsigned int global_depth = 5;
-
-movie_profile* movies[MAX_MOVIES];
-unsigned int* num_of_keyword_matched_movies; // each req has 1 num_of_keyword_matched_movies
-char*** keywords; // each req, each movie has one char* (title) 
-double** scores; // each req, each movie has one score
-
+movie_profile *movies[MAX_MOVIES];
 unsigned int num_of_movies = 0;
 unsigned int num_of_reqs = 0;
+unsigned int num_of_threads = 0;
 
-unsigned int num_of_matched_movies_pserver = 0; // for pserver
+request *reqs[MAX_REQ];
 
-// Global request queue and pointer to front of queue
-// TODO: critical section to protect the global resources
-request* reqs[MAX_REQ];
-int front = -1;
+pthread_t tids[MAX_THREAD];
 
-/* Note that the maximum number of processes per workstation user is 512. * 
- * We recommend that using about <256 threads is enough in this homework. */
-pthread_t tid[MAX_CPU][MAX_THREAD]; //tids for multithread
+void initialize(FILE *fp);
+
+request *read_request();
 
 #ifdef PROCESS
-pid_t pid[MAX_CPU][MAX_THREAD]; //pids for multiprocess
+#define ASSIGN(dest, src) memcpy(dest, src, MAX_LEN)
+#elif defined THREAD
+#define ASSIGN(dest, src) dest = src
 #endif
 
-//mutex
-pthread_mutex_t lock; 
-
-void initialize(FILE* fp);
-request* read_request();
-int pop();
-
-int pop(){
-	front+=1;
-	return front;
+void *create_shared_memory(size_t size)
+{
+	int protection = PROT_READ | PROT_WRITE;
+	int visibility = MAP_SHARED | MAP_ANONYMOUS;
+	return mmap(NULL, size, protection, visibility, -1, 0);
 }
 
-int main(int argc, char *argv[]){
+parameter *genParameter(int depth, int left, int right, recommend *rcmd)
+{
+	parameter *temp = (parameter *)malloc(sizeof(parameter));
+	temp->depth = depth;
+	temp->left = left;
+	temp->right = right;
+	temp->rcmd = rcmd;
+	return temp;
+}
 
-	if(argc != 1){
+recommend *filtering(request *rqst)
+{
 #ifdef PROCESS
-		fprintf(stderr,"usage: ./pserver\n");
+	recommend *data = create_shared_memory(sizeof(recommend));
+	for (int i = 0; i < MAX_MOVIES; i++)
+		data->titles[i] = create_shared_memory(sizeof(char) * MAX_LEN);
 #elif defined THREAD
-		fprintf(stderr,"usage: ./tserver\n");
+	recommend *data = (recommend *)malloc(sizeof(recommend));
+#endif
+
+	bool wildcard = !strcmp(rqst->keywords, "*");
+	int cnt = 0;
+	for (int i = 0; i < num_of_movies; i++)
+	{
+		if (wildcard || strstr(movies[i]->title, rqst->keywords) != NULL)
+		{
+			ASSIGN(data->titles[cnt], movies[i]->title);
+			for (int j = 0; j < NUM_OF_GENRE; j++)
+				data->scores[cnt] += rqst->profile[j] * movies[i]->profile[j];
+			cnt++;
+		}
+	}
+	data->size = cnt;
+	return data;
+}
+
+bool cmp(int i, int j, recommend *rcmd)
+{
+	if (rcmd->scores[i] > rcmd->scores[j])
+		return true;
+	if (rcmd->scores[i] < rcmd->scores[j])
+		return false;
+	if (strcmp(rcmd->titles[i], rcmd->titles[j]) < 0)
+		return true;
+	return false;
+}
+
+void merge(int left, int mid, int right, recommend *rcmd)
+{
+	recommend *temp = (recommend *)malloc(sizeof(recommend));
+	int i = left, j = mid, k = 0;
+
+	while ((i <= mid - 1) && (j <= right))
+	{
+		if (cmp(i, j, rcmd))
+		{
+			temp->titles[k] = rcmd->titles[i];
+			temp->scores[k++] = rcmd->scores[i++];
+		}
+		else
+		{
+			temp->titles[k] = rcmd->titles[j];
+			temp->scores[k++] = rcmd->scores[j++];
+		}
+	}
+	while (i <= mid - 1)
+	{
+		temp->titles[k] = rcmd->titles[i];
+		temp->scores[k++] = rcmd->scores[i++];
+	}
+	while (j <= right)
+	{
+		temp->titles[k] = rcmd->titles[j];
+		temp->scores[k++] = rcmd->scores[j++];
+	}
+	memcpy(rcmd->scores + left, temp->scores, sizeof(double) * (right - left + 1));
+	memcpy(rcmd->titles + left, temp->titles, sizeof(char *) * (right - left + 1));
+	free(temp);
+}
+
+void t_merge_sort(parameter *param)
+{
+	int depth = param->depth;
+	int left = param->left;
+	int right = param->right;
+	recommend *rcmd = param->rcmd;
+
+	if (depth >= MAX_DEPTH || right == left)
+		sort(rcmd->titles + left, rcmd->scores + left, right - left + 1);
+	else
+	{
+		int mid = (left + right) / 2;
+		parameter *param1 = genParameter(depth + 1, left, mid, rcmd);
+		parameter *param2 = genParameter(depth + 1, mid + 1, right, rcmd);
+		if (num_of_threads < MAX_THREAD)
+		{
+			pthread_t tid;
+			num_of_threads++;
+			pthread_create(&tid, NULL, (void *)t_merge_sort, (void *)param1);
+			t_merge_sort(param2);
+			pthread_join(tid, NULL);
+			num_of_threads--;
+		}
+		else
+		{
+			t_merge_sort(param1);
+			t_merge_sort(param2);
+		}
+		merge(left, mid + 1, right, rcmd);
+	}
+}
+
+void t_handle_request(request *rqst)
+{
+	recommend *rcmd = filtering(rqst);
+	parameter *param = genParameter(0, 0, rcmd->size - 1, rcmd);
+	t_merge_sort(param);
+
+	FILE *output;
+	char fileName[32];
+	sprintf(fileName, "%dt.out", rqst->id);
+	if ((output = fopen(fileName, "w+")) == NULL)
+		ERR_EXIT("fopen");
+	for (int i = 0; i < rcmd->size; i++)
+		fprintf(output, "%s\n", rcmd->titles[i]);
+	fclose(output);
+	free(rcmd);
+}
+
+void p_merge_sort(int depth, int left, int right, recommend *rcmd)
+{
+	int len = right - left + 1;
+	if (depth == MAX_DEPTH || right == left)
+	{
+		char **temp_titles = (char **)malloc(sizeof(char *) * len);
+		for (int i = 0; i < len; i++)
+		{
+			temp_titles[i] = (char *)malloc(sizeof(char) * MAX_LEN);
+			memcpy(temp_titles[i], rcmd->titles[i + left], MAX_LEN);
+		}
+		sort(temp_titles, rcmd->scores + left, right - left + 1);
+		for (int i = 0; i < len; i++)
+		{
+			memcpy(rcmd->titles[i + left], temp_titles[i], MAX_LEN);
+			free(temp_titles[i]);
+		}
+		free(temp_titles);
+	}
+	else
+	{
+		int mid = (left + right) / 2;
+		pid_t pid = fork();
+		if (!pid)
+		{
+			p_merge_sort(depth + 1, left, mid, rcmd);
+			exit(0);
+		}
+		else
+		{
+			waitpid(pid, NULL, 0);
+			p_merge_sort(depth + 1, mid + 1, right, rcmd);
+			merge(left, mid + 1, right, rcmd);
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	if (argc != 1)
+	{
+#ifdef PROCESS
+		fprintf(stderr, "usage: ./pserver\n");
+#elif defined THREAD
+		fprintf(stderr, "usage: ./tserver\n");
 #endif
 		exit(-1);
 	}
 
 	FILE *fp;
-
-	if ((fp = fopen("./data/movies.txt","r")) == NULL){
+	if ((fp = fopen("./data/movies.txt", "r")) == NULL)
 		ERR_EXIT("fopen");
-	}
 
 	initialize(fp);
 	assert(fp != NULL);
 	fclose(fp);
 
-	num_of_keyword_matched_movies = calloc(num_of_reqs, sizeof(unsigned int));
-	scores = calloc(num_of_reqs, sizeof(double*));
-	keywords = calloc(num_of_reqs, sizeof(char**));
-	for(int i=0; i<num_of_reqs; i++){
-		scores[i] = calloc(num_of_movies, sizeof(double));
-		keywords[i] = calloc(num_of_movies, sizeof(char*));
+#ifdef PROCESS
+	recommend *rcmd = filtering(reqs[0]);
+	p_merge_sort(1, 0, rcmd->size - 1, rcmd);
+
+	FILE *output;
+	char fileName[32];
+	sprintf(fileName, "%dp.out", reqs[0]->id);
+	if ((output = fopen(fileName, "w")) == NULL)
+		ERR_EXIT("fopen");
+	for (int j = 0; j < rcmd->size; j++)
+		fprintf(output, "%s\n", rcmd->titles[j]);
+
+	fclose(output);
+	munmap(rcmd, sizeof(recommend));
+#elif defined THREAD
+	for (int i = 0; i < num_of_reqs; i++)
+	{
+		num_of_threads++;
+		pthread_create(&tids[i], NULL, (void *)t_handle_request, (void *)reqs[i]);
 	}
 
-	// first filter, then sort
-
-	pthread_t* threads_id = calloc(num_of_reqs, sizeof(pthread_t));
-	thr_fn_args* args = calloc(num_of_reqs, sizeof(thr_fn_args));
-	for(int i=0; i<num_of_reqs; i++){
-#ifdef THREAD
-		args[i].start = 0;
-		args[i].end = 0;
-		args[i].depth = 0;
-		args[i].req_id = i;
-		pthread_create(&threads_id[i], NULL, thr_fn, &args[i]);
-#elif defined PROCESS
-// assert num_of_reqs = 1
-		shmem_sort_handler(0, 0, 0); // end will be assigned in it
-#endif
-	}
-#ifdef THREAD
-	for(int i=0; i<num_of_reqs; i++){
-		pthread_join(threads_id[i], NULL);
-		// write file for thread i
+	for (int i = 0; i < num_of_reqs; i++)
+	{
+		pthread_join(tids[i], NULL);
+		num_of_threads--;
 	}
 #endif
 	return 0;
@@ -97,49 +247,55 @@ int main(int argc, char *argv[]){
  * But feel free if needed.                *
  =========================================**/
 
-request* read_request(){
+request *read_request()
+{
 	int id;
-	char buf1[MAX_LEN],buf2[MAX_LEN];
+	char buf1[MAX_LEN], buf2[MAX_LEN];
 	char delim[2] = ",";
 
 	char *keywords;
 	char *token, *ref_pts;
 	char *ptr;
-	double ret,sum;
+	double ret, sum;
 
-	scanf("%u %254s %254s",&id,buf1,buf2);
-	keywords = malloc(sizeof(char)*strlen(buf1)+1);
-	if(keywords == NULL){
+	scanf("%u %254s %254s", &id, buf1, buf2);
+	keywords = malloc(sizeof(char) * strlen(buf1) + 1);
+	if (keywords == NULL)
+	{
 		ERR_EXIT("malloc");
 	}
 
 	memcpy(keywords, buf1, strlen(buf1));
 	keywords[strlen(buf1)] = '\0';
 
-	double* profile = malloc(sizeof(double)*NUM_OF_GENRE);
-	if(profile == NULL){
+	double *profile = malloc(sizeof(double) * NUM_OF_GENRE);
+	if (profile == NULL)
+	{
 		ERR_EXIT("malloc");
 	}
 	sum = 0;
-	ref_pts = strtok(buf2,delim);
-	for(int i = 0;i < NUM_OF_GENRE;i++){
+	ref_pts = strtok(buf2, delim);
+	for (int i = 0; i < NUM_OF_GENRE; i++)
+	{
 		ret = strtod(ref_pts, &ptr);
 		profile[i] = ret;
-		sum += ret*ret;
-		ref_pts = strtok(NULL,delim);
+		sum += ret * ret;
+		ref_pts = strtok(NULL, delim);
 	}
 
 	// normalize
 	sum = sqrt(sum);
-	for(int i = 0;i < NUM_OF_GENRE; i++){
-		if(sum == 0)
-				profile[i] = 0;
+	for (int i = 0; i < NUM_OF_GENRE; i++)
+	{
+		if (sum == 0)
+			profile[i] = 0;
 		else
-				profile[i] /= sum;
+			profile[i] /= sum;
 	}
 
-	request* r = malloc(sizeof(request));
-	if(r == NULL){
+	request *r = malloc(sizeof(request));
+	if (r == NULL)
+	{
 		ERR_EXIT("malloc");
 	}
 
@@ -151,70 +307,78 @@ request* read_request(){
 }
 
 /*=================initialize the dataset=================*/
-void initialize(FILE* fp){
+void initialize(FILE *fp)
+{
 
 	char chunk[MAX_LEN] = {0};
-	char *token,*ptr;
-	double ret,sum;
+	char *token, *ptr;
+	double ret, sum;
 	int cnt = 0;
 
 	assert(fp != NULL);
 
 	// first row
-	if(fgets(chunk,sizeof(chunk),fp) == NULL){
+	if (fgets(chunk, sizeof(chunk), fp) == NULL)
+	{
 		ERR_EXIT("fgets");
 	}
 
-	memset(movies,0,sizeof(movie_profile*)*MAX_MOVIES);	
+	memset(movies, 0, sizeof(movie_profile *) * MAX_MOVIES);
 
-	while(fgets(chunk,sizeof(chunk),fp) != NULL){
-		
+	while (fgets(chunk, sizeof(chunk), fp) != NULL)
+	{
+
 		assert(cnt < MAX_MOVIES);
-		chunk[MAX_LEN-1] = '\0';
+		chunk[MAX_LEN - 1] = '\0';
 
-		const char delim1[2] = " "; 
+		const char delim1[2] = " ";
 		const char delim2[2] = "{";
 		const char delim3[2] = ",";
 		unsigned int movieId;
-		movieId = atoi(strtok(chunk,delim1));
+		movieId = atoi(strtok(chunk, delim1));
 
 		// title
-		token = strtok(NULL,delim2);
-		char* title = malloc(sizeof(char)*strlen(token)+1);
-		if(title == NULL){
+		token = strtok(NULL, delim2);
+		char *title = malloc(sizeof(char) * strlen(token) + 1);
+		if (title == NULL)
+		{
 			ERR_EXIT("malloc");
 		}
-		
+
 		// title.strip()
-		memcpy(title, token, strlen(token)-1);
-	 	title[strlen(token)-1] = '\0';
+		memcpy(title, token, strlen(token) - 1);
+		title[strlen(token) - 1] = '\0';
 
 		// genres
-		double* profile = malloc(sizeof(double)*NUM_OF_GENRE);
-		if(profile == NULL){
+		double *profile = malloc(sizeof(double) * NUM_OF_GENRE);
+		if (profile == NULL)
+		{
 			ERR_EXIT("malloc");
 		}
 
 		sum = 0;
-		token = strtok(NULL,delim3);
-		for(int i = 0; i < NUM_OF_GENRE; i++){
+		token = strtok(NULL, delim3);
+		for (int i = 0; i < NUM_OF_GENRE; i++)
+		{
 			ret = strtod(token, &ptr);
 			profile[i] = ret;
-			sum += ret*ret;
-			token = strtok(NULL,delim3);
+			sum += ret * ret;
+			token = strtok(NULL, delim3);
 		}
 
 		// normalize
 		sum = sqrt(sum);
-		for(int i = 0; i < NUM_OF_GENRE; i++){
-			if(sum == 0)
+		for (int i = 0; i < NUM_OF_GENRE; i++)
+		{
+			if (sum == 0)
 				profile[i] = 0;
 			else
 				profile[i] /= sum;
 		}
 
-		movie_profile* m = malloc(sizeof(movie_profile));
-		if(m == NULL){
+		movie_profile *m = malloc(sizeof(movie_profile));
+		if (m == NULL)
+		{
 			ERR_EXIT("malloc");
 		}
 
@@ -227,9 +391,10 @@ void initialize(FILE* fp){
 	num_of_movies = cnt;
 
 	// request
-	scanf("%d",&num_of_reqs);
+	scanf("%d", &num_of_reqs);
 	assert(num_of_reqs <= MAX_REQ);
-	for(int i = 0; i < num_of_reqs; i++){
+	for (int i = 0; i < num_of_reqs; i++)
+	{
 		reqs[i] = read_request();
 	}
 }
